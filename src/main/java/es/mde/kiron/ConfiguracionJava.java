004f66ce76001bd31b57c9be91ee96d9838bde14
imunnic <imunnic1@gmail.com>
2024-10-04 11:21:49 +0200

    cambios para despliegue

--- src/main/java/es/mde/kiron/ConfiguracionJava.java
+++ src/main/java/es/mde/kiron/ConfiguracionJava.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource(
     {
         "classpath:configuracion/gestionDB.properties",
-        "classpath:configuracion/secret.properties",
+//        "classpath:configuracion/secret.properties",
 
     })
 public class ConfiguracionJava {
